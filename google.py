15489ce1405b164fa465a836b90a1b00620cd77b
LeeHannaa <103620720+LeeHannaa@users.noreply.github.com>
2024-10-24 02:05:15 +0900

    feat: cors 에러 해결

--- google.py
+++ google.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
 from datetime import datetime
 from pytrends.request import TrendReq
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Google Trends API 연결
 pytrends = TrendReq(hl='ko', tz=540)
